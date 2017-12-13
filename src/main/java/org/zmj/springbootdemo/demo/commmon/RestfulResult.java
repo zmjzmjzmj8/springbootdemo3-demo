17d51f4600e7d5d07e28cf25a5c8003a79f73826
Zmj <zmjzmjzmj33@sina.com>
2017-12-13 11:40:47 +0800

    新增统一返回类

---
+++ src/main/java/org/zmj/springbootdemo/demo/commmon/RestfulResult.java
@@ -0,0 +1,47 @@
+package org.zmj.springbootdemo.demo.commmon;
+
+/**
+ * http返回restful对象
+ * @author zmj
+ */
+public class RestfulResult<T> {
+
+    /**
+     * 状态码
+     */
+    private Integer status;
+
+    /**
+     * 提示信息
+     */
+    private String msg;
+
+    /**
+     * 具体内容
+     */
+    private T result;
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public T getResult() {
+        return result;
+    }
+
+    public void setResult(T result) {
+        this.result = result;
+    }
+}
---
+++ src/main/java/org/zmj/springbootdemo/demo/utils/RestfulResultUtils.java
@@ -0,0 +1,45 @@
+package org.zmj.springbootdemo.demo.utils;
+
+import org.zmj.springbootdemo.demo.commmon.RestfulResult;
+
+/**
+ * RestfulResult对象工厂工具
+ * @author zmj
+ */
+public class RestfulResultUtils {
+
+    /**
+     * 返回成功
+     * @param object
+     * @return
+     */
+    public static RestfulResult success(Object object){
+        RestfulResult restfulResult = new RestfulResult();
+        restfulResult.setStatus(SysCode.SYS_CODE_STATUS_SUCCESS.getCode());
+        restfulResult.setMsg(SysCode.SYS_CODE_STATUS_SUCCESS.getDescription());
+        restfulResult.setResult(object);
+        return restfulResult;
+    }
+
+    /**
+     * 返回成功，并且无返回实体
+     * @return
+     */
+    public static RestfulResult success(){
+        return success(null);
+    }
+
+    /**
+     * 返回失败
+     * @param code
+     * @param msg
+     * @return
+     */
+    public static RestfulResult error(Integer code,String msg){
+        RestfulResult restfulResult = new RestfulResult();
+        restfulResult.setStatus(code);
+        restfulResult.setMsg(msg);
+        restfulResult.setResult(null);
+        return restfulResult;
+    }
+}
